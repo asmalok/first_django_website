5ad5c1306881e9acf508d3137579c39a58f4e283
Asma Lokhandwala <asma@osprey.acadiau.ca>
2019-05-09 15:56:42 -0300

    Basic Structure Done

---
+++ django-practice-app/v_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/asma/Documents/Work/DjangoProjects/django-practice-app/v_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
