f7ce98d52df64e07a043a6d17739a38804404501
Asma Lokhandwala <asma@osprey.acadiau.ca>
2019-05-15 09:54:35 -0300

    Tried chaning views. Didn't work so reverted back

--- django-practice-app/shop/models.py
+++ django-practice-app/shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Type(models.Model):
+    type_clothing = models.CharField(max_length = 10)
+
+    def __str__(self):
+        return self.type_clothing
--- django-practice-app/shop/views.py
+++ django-practice-app/shop/views.py
@@ -4,9 +4,22 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Type
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class ClothView(generic.DetailView):
+    model = Type
+    template_name = 'cloth.html'
+    
+    # def get(self, request):
+    #     print(self)
+    #     return HttpResponse(self.type)
+
+    # def redirect_to_specified_clothing(request):
+    #     return HttpResponseRedirect(reverse('pants', args=(pants)))
